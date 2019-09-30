1602c24d4b8ebcfc2645091619fb0ad18fbde1b2
Cryosis-stat <josepabloms@gmail.com>
2019-09-30 15:52:58 -0600

    agregacion de los sp

---
+++ SP/dbo.agregarBeneficiario.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE agregarBeneficiario
+	@nombre varchar(50),
+	@fecha date,
+	@porcentaje INT,
+    @parentesco VARCHAR(50),
+	@docId VARCHAR(50),
+	@email VARCHAR(50),
+	@telefono1 VARCHAR(50),
+	@telefono2 VARCHAR(50),
+	@tipoDoc INT,
+	@numCuenta INT
+AS
+	BEGIN
+		BEGIN TRY 
+			INSERT INTO Beneficiario (Nombre, PorcentajeBeneficio, Activo, FechaNacimiento,	Parentesco, 
+									DocId, Email, Telefono1, Telefono2, TipoDocId, IdCuenta)
+			VALUES (@nombre, @porcentaje, 1, @fecha)
+			RETURN @@ROWCOUNT
+		END TRY
+		BEGIN CATCH
+			RETURN @@ERROR *-1
+		END CATCH
+	END
---
+++ SP/dbo.editarBeneficiarios.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE editarBeneficiarios 
+	@nombre VARCHAR(50),
+	@porcentaje INT,
+	@parentesco VARCHAR(50)
+AS
+	BEGIN 
+		BEGIN TRY
+			UPDATE Beneficiario
+			SET Nombre = @nombre, Parentesco = @parentesco, PorcentajeBeneficio = @porcentaje
+			WHERE (Activo = 1) 
+				
+
+			RETURN @@ROWCOUNT
+		END TRY
+        BEGIN CATCH 
+            RETURN @@ERROR *-1
+        END CATCH 
+    END
